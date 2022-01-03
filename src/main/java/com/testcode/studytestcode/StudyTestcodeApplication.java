5a0df6baf5d2da36c2ab0b73ae82c3a714100bac
hyeongwoo-LEE <lhw970804@naver.com>
2022-01-03 15:34:55 +0900

    init

---
+++ src/main/java/com/testcode/studytestcode/StudyTestcodeApplication.java
@@ -0,0 +1,13 @@
+package com.testcode.studytestcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTestcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyTestcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testcode/studytestcode/StudyTestcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testcode.studytestcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTestcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
